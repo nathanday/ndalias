3031a76169fc59d8ca4b61d380a6a35c96204f49
Sean McBride <sean@rogue-research.com>
2012-04-13 20:06:39 -0400

    fixed minor compiler warning

--- Classes/NDAlias.m
+++ Classes/NDAlias.m
@@ -448,7 +448,7 @@ static NSData * NDDataForAliasHandle (AliasHandle anAliasHandle)
  */
 - (NSString *)debugDescription
 {
-	NSString * str = [NSString stringWithFormat:@"aliasHandle %p, changed %d, mountFlags %x, lastKnownPath %@",
+	NSString * str = [NSString stringWithFormat:@"aliasHandle %p, changed %d, mountFlags %lx, lastKnownPath %@",
 					  aliasHandle,
 					  changed,
 					  mountFlags,
